6a59b51ee8e31af32d5ba7b18b2f774cc2459668
LiamH <liamhearts@gmail.com>
2019-04-10 12:54:14 -0700

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//TODO: make questions/answers appear when the user hits start
+//TODO: start a timer when the user hits start
+
+function start(){
+ $("#test").show()
+    }
+$("#test").hide();
+$("#start").on("click", start);
